58d2b80488e5d384c1d1340c012764404ce4f900
Nicklas Di Pietro <Nicklasdipietro@gmail.com>
2018-11-08 16:33:32 -0800

    Test.

--- src/main/java/D4/MinMaxArray.java
+++ src/main/java/D4/MinMaxArray.java
@@ -1,28 +1,28 @@
-package D4;
-
-public class MinMaxArray {
-    public static void main(String[] args) {
-        MinMaxArray(int[] arr);
-    }
-// Given an array of 5 integers, find the minimum and maximum number possible in the array.
-
-// min = 1
-// max = 9
-    public static void MinMaxArray(int[] arr) {
-
-        long min = 0;
-        long max = 0;
-
-        for (int i = 0; i < arr.length - 1; i++) {
-            min += arr[i];
-        }
-
-        for (int j = arr.length - 1; j > 0; j--)
-            max += arr[j];
-
-        System.out.print(min + " " + max);
-    }
-
-}
+//package D4;
+//
+//public class MinMaxArray {
+//    public static void main(String[] args) {
+//        MinMaxArray(int[] arr);
+//    }
+//// Given an array of 5 integers, find the minimum and maximum number possible in the array.
+//
+//// min = 1
+//// max = 9
+//    public static void MinMaxArray(int[] arr) {
+//
+//        long min = 0;
+//        long max = 0;
+//
+//        for (int i = 0; i < arr.length - 1; i++) {
+//            min += arr[i];
+//        }
+//
+//        for (int j = arr.length - 1; j > 0; j--)
+//            max += arr[j];
+//
+//        System.out.print(min + " " + max);
+//    }
+//
+//}
 
 
